2d0dbee72c5732d19cbbc43d26f80b09da8d0b80
yueuni <eun@kakao.com>
2023-01-30 01:00:56 +0900

    [FEAT] LOG winston 셋팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger as WisLog } from 'winston';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly log2: WisLog
+    , private readonly appService: AppService
+  ) {
+    this.log2.error('title');
+    this.log2.debug('title');
+    this.log2.info('yo', {a: '1'});
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { utilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import * as moment from 'moment';
+
+const env = process.env.NODE_ENV;
+const loggerLever =  env === 'PROD' ? 'info' : 'debug';
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({  
+          handleExceptions: true,
+          level: loggerLever,
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike('Payco-bot', {
+                prettyPrint: true,
+            }),
+            winston.format.colorize({ all: true })
+          )
+        }),
+        new winston.transports.File({ 
+          dirname: `./logs`,
+          filename: `${moment(new Date()).format('YYYY-MM-DD')}.log`,
+          level: loggerLever,
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike('Payco-bot', {
+                prettyPrint: true,
+            })
+          ),
+          maxFiles: 30
+        }),
+      ],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule{
+}
